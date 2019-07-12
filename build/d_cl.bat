eaca2fa6349a68bd583f1d391446af74ba6444c1
Dan Engelbrecht <dan.engelbrecht@king.com>
2019-07-12 23:24:26 +0200

    cl.exe build fixes

--- build/d_cl.bat
+++ build/d_cl.bat
@@ -5,6 +5,7 @@ set CXXFLAGS=
 set OUTPUT=test_debug
 
 call build_src.bat
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
 call build\mvsc_build.bat
 exit /B %ERRORLEVEL%
--- build/r_cl.bat
+++ build/r_cl.bat
@@ -4,5 +4,8 @@ set OPT=/O2
 set CXXFLAGS=
 set OUTPUT=test
 
-call build\build_cl.bat
+call build_src.bat
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
+call build\mvsc_build.bat
 exit /B %ERRORLEVEL%
