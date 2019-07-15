09c9b1a6503b2671a62d958e1808af331875f6a8
Dan Engelbrecht <dan@engelbrecht.com>
2019-07-15 18:17:53 +0200

    build script refactorings (#2)

--- build_src.bat
+++ build_src.bat
@@ -1,3 +1,5 @@
 set SRC=src/trove.cpp
 set TEST_SRC=test\test.cpp test\main.cpp
 set THIRDPARTY_SRC=
+set CXXFLAGS=%CXXFLAGS%
+set CXXFLAGS_DEBUG=%CXXFLAGS_DEBUG%
--- build_src.sh
+++ build_src.sh
@@ -3,3 +3,5 @@
 export SRC="src/trove.cpp"
 export TEST_SRC="test/main.cpp test/test.cpp"
 export THIRDPARTY_SRC=
+export CXXFLAGS="$CXXFLAGS"
+export CXXFLAGS_DEBUG="$CXXFLAGS_DEBUG"
